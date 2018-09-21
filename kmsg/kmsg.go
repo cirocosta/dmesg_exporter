2c806adfa2e990be94696087519d55215850268c
Ciro S. Costa <ciro9758@gmail.com>
2018-09-21 18:15:24 +0000

    Adds priority and facility validation

--- kmsg/kmsg.go
+++ kmsg/kmsg.go
@@ -3,24 +3,31 @@
 package kmsg
 
 import (
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/pkg/errors"
 )
 
-type Level uint8
+type Priority uint8
 
 const (
-	LevelEmerg Level = iota
-	LevelAlert
-	LevelCrit
-	LevelErr
-	LevelWarning
-	LevelNotice
-	LevelInfo
-	LevelDebug
+	PriorityEmerg Priority = iota
+	PriorityAlert
+	PriorityCrit
+	PriorityErr
+	PriorityWarning
+	PriorityNotice
+	PriorityInfo
+	PriorityDebug
+	priorityEnd
 )
+
+func IsValidPriority (priority uint8) (isValid bool) {
+	isValid = (priority < uint8(priorityEnd))
+	return
+}
 
 type Facility uint8
 
@@ -33,10 +40,16 @@ const (
 	FacilitySyslog
 	FacilityLpr
 	FacilityNews
+	facilityEnd
 )
 
+func IsValidFacility (facility uint8) (isValid bool) {
+	isValid = (facility < uint8(facilityEnd))
+	return
+}
+
 type Message struct {
-	Level          Level
+	Priority          Priority
 	Facility       Facility
 	SequenceNumber int64
 	Timestamp      time.Time
@@ -44,16 +57,31 @@ type Message struct {
 	Metadata       map[string]string
 }
 
+// DecodePrefix extracts both priority and facility from a given
+// syslog(2) encoded prefix.
+//
+//	    facility    priority
+//      .--------------.-----.
+//      |              |     |
+//	7  6  5  4  3  2  1  0    bits
+//
+//
+func DecodePrefix(prefix uint8) (priority Priority, facility Facility, err error) {
+	priority = Priority((prefix << (8-3)) >> 8-3)
+	facility = Facility(prefix >> 3)
+
+	return
+}
+
 // Parse takes care of parsing a `kmsg` message acording to the kernel
 // documentation at https://www.kernel.org/doc/Documentation/ABI/testing/dev-kmsg.
 //
 // REGULAR MESSAGE:
 //
-//                  INFO												       MSG
+//                  INFO		              MSG
 //     .------------------------------------------. .------.
-//    |																		         |			  |
-//    |																		         |			  |
-//    |	int	    int      int       char, <ignore>  | string |
+//    |                                            |        |
+//    |	int	int      int      char, <ignore>   | string |
 //    priority, seq, timestamp_us,flag[,..........];<message>
 //
 //
@@ -82,6 +110,13 @@ func Parse(rawMsg string) (m *Message, err error) {
 	splittedInfoSection := strings.SplitN(infoSection, ",", 5)
 	if len(splittedInfoSection) < 4 {
 		err = errors.Errorf("info section with not enought fields")
+		return
+	}
+
+	_, err = strconv.Atoi(splittedInfoSection[0])
+	if err != nil {
+		err = errors.Wrapf(err,
+			"couldn't convert priority to int")
 		return
 	}
 
--- kmsg/kmsg_test.go
+++ kmsg/kmsg_test.go
@@ -8,6 +8,48 @@ import (
 )
 
 var _ = Describe("Kmsg", func() {
+	Describe("IsValidFacility", func() {
+		It("validates if the facility is valid or not", func() {
+			var (
+				valid = []uint8{
+					1, 2, 3, 4, 5, 6,
+				}
+				invalid = []uint8{
+					30, 31, 230, 111,
+				}
+			)
+
+			for _, facility := range valid {
+				Expect(kmsg.IsValidFacility(facility)).To(BeTrue())
+			}
+
+			for _, facility := range invalid {
+				Expect(kmsg.IsValidFacility(facility)).ToNot(BeTrue())
+			}
+		})
+	})
+
+	Describe("IsValidPriority", func() {
+		It("validates if the priority is valid or not", func() {
+			var (
+				valid = []uint8{
+					1, 2, 3, 4, 5, 6,
+				}
+				invalid = []uint8{
+					30, 31, 230, 111,
+				}
+			)
+
+			for _, priority := range valid {
+				Expect(kmsg.IsValidPriority(priority)).To(BeTrue())
+			}
+
+			for _, priority := range invalid {
+				Expect(kmsg.IsValidPriority(priority)).ToNot(BeTrue())
+			}
+		})
+	})
+
 	Describe("Parse", func() {
 		var (
 			input string
@@ -48,6 +90,15 @@ var _ = Describe("Kmsg", func() {
 				})
 			})
 
+			Context("not having a valid priority field", func() {
+				BeforeEach(func() {
+					input = "aa;aa"
+				})
+
+				It("fails", func() {
+					Expect(err).To(HaveOccurred())
+				})
+			})
 		})
 	})
 })
